10feaa0dbe9978765956db7c1842f68af4910be5
ZooM982 <haurlyroll@gmail.com>
2024-10-10 17:09:44 +0000

    test

--- frontend/src/components/Products.js
+++ frontend/src/components/Products.js
@@ -10,7 +10,7 @@ function Products({ addToCart }) {
     const fetchProducts = async () => {
       try {
         // Si une catégorie est spécifiée, passer cette catégorie dans la requête API
-        const response = await axios.get(`http://localhost:5000/api/products`, {
+        const response = await axios.get(`https://ecom-lb.onrender.com/api/products`, {
           params: {
             category: category || '' // Envoyer la catégorie dans la requête
           }

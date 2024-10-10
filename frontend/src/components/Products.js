e5d98d9cdcc9ceae5be85d6bf7f8f4187532ba77
ZooM982 <haurlyroll@gmail.com>
2024-10-10 16:18:12 +0000

    test

--- frontend/src/components/Products.js
+++ frontend/src/components/Products.js
@@ -10,7 +10,7 @@ function Products({ addToCart }) {
     const fetchProducts = async () => {
       try {
         // Si une catégorie est spécifiée, passer cette catégorie dans la requête API
-        const response = await axios.get(`http://localhost:5000/api/products`, {
+        const response = await axios.get(`ecom-lb.vercel.app/api/products`, {
           params: {
             category: category || '' // Envoyer la catégorie dans la requête
           }

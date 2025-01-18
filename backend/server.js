9027e9ddccc0ef6ab562987886aece7fcf6e8b03
Revhieno Roll Haurly MBOUTA <haurlyroll@icloud.com>
2025-01-18 11:20:29 +0000

    test

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,12 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const dotenv = require('dotenv');
-const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('./docs/swagger.json');
-const productRoutes = require('./routes/productRoutes');
-const userRoutes = require('./routes/userRoutes');
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./docs/swagger.json");
+const productRoutes = require("./routes/productRoutes");
+const userRoutes = require("./routes/userRoutes");
+const path = require("path"); // Importation du module path pour gérer les chemins
 
 dotenv.config();
 
@@ -13,23 +14,28 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  serverSelectionTimeoutMS: 60000, // 60 secondes
-})
-  .then(() => console.log('MongoDB connected'))
-  .catch(err => console.error('Erreur de connexion à MongoDB :', err));
+// Connexion à MongoDB
+mongoose
+	.connect(process.env.MONGODB_URI, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+		serverSelectionTimeoutMS: 60000, // 60 secondes
+	})
+	.then(() => console.log("MongoDB connected"))
+	.catch((err) => console.error("Erreur de connexion à MongoDB :", err));
+
+// Configuration pour servir les fichiers statiques depuis le dossier uploads
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 // Utiliser les routes
-app.use('/api/products', productRoutes);
-app.use('/api/users', userRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
 
 // Documentation de l'API
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
+// Démarrer le serveur
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+	console.log(`Server is running on port ${PORT}`);
 });

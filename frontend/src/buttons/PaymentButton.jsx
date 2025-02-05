74f3e2e60d7fce0ec2b987413a5eb486b2ffce47
Revhieno Roll Haurly MBOUTA <haurlyroll@icloud.com>
2025-02-05 14:09:19 +0000

    logo paydunya

--- frontend/src/buttons/PaymentButton.jsx
+++ frontend/src/buttons/PaymentButton.jsx
@@ -1,35 +1,42 @@
 import React from "react";
 import axios from "axios";
+import { PiPaypalLogoLight } from "react-icons/pi";
 
 const PaymentButton = ({ orderId, name, email, amount }) => {
-  const handlePayment = async () => {
-    try {
-      const response = await axios.post("https://haurly-shop.onrender.com/api/payments", {
-        orderId,
-        name,
-        email,
-        amount,
-      });
+	const handlePayment = async () => {
+		try {
+			const response = await axios.post(
+				"https://haurly-shop.onrender.com/api/payments",
+				{
+					orderId,
+					name,
+					email,
+					amount,
+				}
+			);
 
-      if (response.data.redirectUrl) {
-        window.location.href = response.data.redirectUrl;
-      } else {
-        alert("Erreur lors du paiement !");
-      }
-    } catch (error) {
-      console.error("Erreur paiement:", error);
-      alert("Une erreur s'est produite. Réessayez plus tard.");
-    }
-  };
+			if (response.data.redirectUrl) {
+				window.location.href = response.data.redirectUrl;
+			} else {
+				alert("Erreur lors du paiement !");
+			}
+		} catch (error) {
+			console.error("Erreur paiement:", error);
+			alert("Une erreur s'est produite. Réessayez plus tard.");
+		}
+	};
 
-  return (
-    <button 
-      onClick={handlePayment} 
-      className="bg-blue-600 text-white text-center p-2 h-[40px] flex justify-between ps-3 items-center rounded"
-    >
-      <p className="hidden md:inline-block text-[20px]">Payer par PayDunya</p>
-    </button>
-  );
+	return (
+		<button
+			onClick={handlePayment}
+			className="bg-blue-600 text-white text-center p-2 h-[40px] flex justify-between ps-3 items-center rounded"
+		>
+			<p className="hidden md:inline-block text-[20px]">Payer par PayDunya</p>
+			<span className="text-white text-[40px] mx-auto">
+				<PiPaypalLogoLight />
+			</span>
+		</button>
+	);
 };
 
 export default PaymentButton;
